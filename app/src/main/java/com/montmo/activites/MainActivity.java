df9347ed8d011cbcfd9c00495f8ba2d14da50e81
Julien Canuel <jul_soccer7@hotmail.com>
2016-12-01 17:05:23 -0500

    Méthode menuDrawerLayout commencer pour le menu à gauche

--- app/src/main/java/com/montmo/activites/MainActivity.java
+++ app/src/main/java/com/montmo/activites/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuDrawerLayour();
+
+
+    }
+    private ListView listView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
+    //Méthode pour tester la création d'un menu glissant à gauche de l'application
+    private void menuDrawerLayour() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        listView = (ListView)findViewById(R.id.list_navigation);
+
+        //Lier le drawerLayout à la barred'actions.
+        //Utilisation de la flèche de remontée pour afficher ;e menu.
+        // Le constructeur prend en paramètres : l'activité qui accueille le drawer, le drawerLayout
+        // et deux chaines utilisées à l'ouvertures et à la fermeture du menu (pour l'accessibilité).
 
     }
 }
